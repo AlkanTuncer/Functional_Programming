667418367603c16398b5b31e3f98093de7d31d96
AlkanTuncer <alkantuncer53@hotmail.com>
2022-06-07 22:14:36 +0300

    Functional Programming - Start

--- src/day068/Main_LambdaReturn.java
+++ src/day068/Main_LambdaReturn.java
@@ -3,7 +3,7 @@ package day068;
 public class Main_LambdaReturn {
     public static void main(String[] args) {
 
-        Square kare = (v) -> v*v;
+        Square kare = (v) -> v * v;
         int calculate = kare.calculate(5);
         System.out.println(calculate);
 
@@ -11,7 +11,7 @@ public class Main_LambdaReturn {
         Square kare2 = new Square() {     // Yukarıda tek satırda override ettik (:
             @Override
             public int calculate(int value) {
-                return value*value;
+                return value * value;
             }
         };
 
